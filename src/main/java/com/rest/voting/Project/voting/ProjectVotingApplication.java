8ad3417024541492c41232e2a84b766baf5f1325
Przemysław Hendel <przem1361@gmail.com>
2019-03-07 14:06:44 +0100

    spring boot initializr project

---
+++ src/main/java/com/rest/voting/Project/voting/ProjectVotingApplication.java
@@ -0,0 +1,13 @@
+package com.rest.voting.Project.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVotingApplication.class, args);
+	}
+
+}
