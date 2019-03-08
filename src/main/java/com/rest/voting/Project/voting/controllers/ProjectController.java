745b33e2ec6459e9acf400729824f366d65c74b1
Przemysław Hendel <przem1361@gmail.com>
2019-03-08 08:28:43 +0100

    Optional project method for retrieving single projects by ID, using a PathVariable --possible change to @GetMapping

--- src/main/java/com/rest/voting/Project/voting/controllers/ProjectController.java
+++ src/main/java/com/rest/voting/Project/voting/controllers/ProjectController.java
@@ -4,12 +4,16 @@ import com.rest.voting.Project.voting.model.Project;
 import com.rest.voting.Project.voting.repositories.ProjectRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
+//TODO - information if project is open for voting,
+//TODO 	- and the number of votes for and against it
 @Controller
 public class ProjectController {
     private ProjectRepository projectRepository;
@@ -22,8 +26,16 @@ public class ProjectController {
     public @ResponseBody
     List<Project> getProjects() {
         List<Project> projects = projectRepository.findAll(new Sort(Sort.Direction.ASC, "name"));
-
         return projects;
     }
 
+
+    @RequestMapping(value = "/projectt/{id}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    Optional<Project> getProjectById(@PathVariable Long id) {
+
+        return projectRepository.findById(id);
+
+    }
 }
+
