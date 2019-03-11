aae42540b5e686b8c0e2c644190a6d1eac1323be
Przemysław Hendel <przem1361@gmail.com>
2019-03-11 09:53:51 +0100

    @Autowired for dependency injection instead of a constructor

--- src/main/java/com/rest/voting/Project/voting/bootstrap/DevBootstrap.java
+++ src/main/java/com/rest/voting/Project/voting/bootstrap/DevBootstrap.java
@@ -4,19 +4,17 @@ import com.rest.voting.Project.voting.model.Project;
 import com.rest.voting.Project.voting.model.Voter;
 import com.rest.voting.Project.voting.repositories.ProjectRepository;
 import com.rest.voting.Project.voting.repositories.VoterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
     private ProjectRepository projectRepository;
+    @Autowired
     private VoterRepository voterRepository;
-
-    public DevBootstrap(ProjectRepository projectRepository, VoterRepository voterRepository) {
-        this.projectRepository = projectRepository;
-        this.voterRepository = voterRepository;
-    }
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
